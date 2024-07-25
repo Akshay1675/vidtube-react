d74c623079d3cab8f24691bbc5172f4fcf3e2479
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 15:05:58 +0530

    dark mode updates

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const LiveChat = ({name, message, img}) => {
-    
+const LiveChat = ({ name, message, img }) => {
   return (
-    <div className='py-2 hover:bg-slate-200'>
-        <div className='flex '>
-        <img className='h-8 rounded-full' alt='avtar' src={img?img : "https://randomuser.me/api/portraits/men/12.jpg"}/>
-        <span className="ml-2"><span className='font-bold px-2'>{name}</span> {message}</span>
-        
-        </div>
-    
-      
+    <div className="py-2 hover:bg-slate-200 dark:hover:bg-[#3c3c3c]">
+      <div className="flex ">
+        <img
+          className="h-8 rounded-full"
+          alt="avtar"
+          src={img ? img : "https://randomuser.me/api/portraits/men/12.jpg"}
+        />
+        <span className="ml-2 dark:text-[#fff] ">
+          <span className="font-bold px-2 dark:text-[#fcfcfc]">{name}</span>{" "}
+          {message}
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LiveChat
+export default LiveChat;
