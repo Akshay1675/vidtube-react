7ceeb0cedb7dfc147cc39ab373f6254b18f73c42
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 15:50:59 +0530

    Update VideoContainer.js

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,45 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import { TAB_VIDEO_URL, YOUTUBE_API_KEY, YOUTUBE_VIDEO_URL } from '../utils/constants'
-import VideoCard from "./VideoCard"
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import Shimmer from './Shimmer'
-
+import React from "react";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Shimmer from "./Shimmer";
+import useInfiniteScroll from "../utils/hooks/useInfiniteScroll";
+import useVideoList from "../utils/hooks/useVideoList";
 
 const VideoContainer = () => {
+  const currentTab = useSelector((store) => store.app.mainContainerStatus);
+  const Sidebar = useSelector((store) => store.app.isMenuOpen);
 
-  const currentTab = useSelector(store => store.app.mainContainerStatus)
-  const [videoList, setVideoList] = useState(null)
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    (currentTab === "All") ? getVideos() : tabVideos()
-  },[currentTab]) 
-   const getVideos = async () => {
-     setLoading(true)
-      const data = await fetch(YOUTUBE_VIDEO_URL)
-      const json = await data.json()
-      setVideoList(json.items)
-      setLoading(false)
-   }
+  const { tabVideos, getVideos, nextPageToken, videoList, loading } =
+    useVideoList();
 
-   const tabVideos = async () => {
-    setLoading(true)
-      const data = await fetch(TAB_VIDEO_URL + currentTab +  " &type=video&key=" + YOUTUBE_API_KEY)
-      const json = await data.json()
-      setVideoList(json.items)
-      setLoading(false)
+  useInfiniteScroll(tabVideos, getVideos, nextPageToken);
 
-      
-   }
+  if (videoList.length < 0) {
+    return <Shimmer />;
+  }
 
-
-   
-  return loading ? <Shimmer /> : (
-    <div className=" md:ml-2 md:flex md:flex-wrap ">
-      {videoList?.map((video,i) => <Link key={video.id} to={(currentTab === "All") ? "/watch?v=" +video.id : "/watch?v=" +video.id.videoId}><VideoCard  info={video} /></Link>)}
+  return loading ? (
+    <Shimmer />
+  ) : (
+    <div
+      className={`${
+        !Sidebar ? "md:ml-[4.5rem]" : ""
+      } md:ml-2 md:flex md:flex-wrap ml-5`}
+    >
+      {videoList?.map((video, i) => (
+        <Link
+          key={video.id}
+          to={
+            currentTab === "All"
+              ? "/watch?v=" + video.id
+              : "/watch?v=" + video.id.videoId
+          }
+        >
+          <VideoCard info={video} />
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default VideoContainer
-       
+export default VideoContainer;
