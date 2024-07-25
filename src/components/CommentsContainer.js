1a93c767d625e55f4fbd281201e66db1b77d8219
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 12:38:57 +0530

    Update CommentsContainer.js

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,15 +1,14 @@
-import CommentList from "./CommentList"
-import { commentsData } from "../utils/commentData"
-
+import CommentList from "./CommentList";
+import { commentsData } from "../utils/commentData";
 
 const CommentsContainer = () => {
-    
   return (
     <div className="px-4 mx-4 mt-2">
-      <h1 className="font-bold text-xl">Comment :</h1>
-      <CommentList comments={ commentsData }/>
+      <h1 className="font-bold text-xl dark:text-[#fff]">Comment :</h1>
+      <CommentList comments={commentsData} />
     </div>
-  )
-}
+  );
+};
 
-export default CommentsContainer
+export default CommentsContainer;
+
