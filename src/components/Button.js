67b71a45622193758a833781064ce8355018fdd8
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 12:32:30 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { changeTab } from '../utils/appSlice'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeTab } from "../utils/appSlice";
 
-const Button = ({name}) => {
-  const currentTab = useSelector(store => store.app.mainContainerStatus)
-  const dispatch = useDispatch()
-  
+const Button = ({ name }) => {
+  const currentTab = useSelector((store) => store.app.mainContainerStatus);
+  const dispatch = useDispatch();
+
   const handleClick = (b) => {
-    dispatch(changeTab(b))
-  }
+    dispatch(changeTab(b));
+  };
   return (
-    <div className=''>
-      <button onClick={() => handleClick(name)} className={`px-6 py-1 mx-2 my-2 rounded-md cursor-pointer ${currentTab === name ? 'bg-black text-white' : 'bg-gray-200 hover:bg-pink-800 hover:text-white text-gray-700'}`}
->{name}</button>
+    <div className="">
+      <button
+        onClick={() => handleClick(name)}
+        className={`px-6 py-1 mx-2 my-2 rounded-md cursor-pointer ${
+          currentTab === name
+            ? "bg-black dark:bg-gray-300 dark:text-black text-white"
+            : "bg-gray-200 dark:bg-[#3c3c3c] dark:text-[#fff] dark:hover:bg-pink-800 hover:bg-pink-800 hover:text-white text-gray-700"
+        }`}
+      >
+        {name}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
