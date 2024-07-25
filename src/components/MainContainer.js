c36e110c3946ade808c718810e13c7c3a7e88885
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 15:12:33 +0530

    dark mode updates

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,21 +1,21 @@
-import React, { useEffect } from 'react'
-import ButtonList from './ButtonList'
-import VideoContainer from './VideoContainer'
-import { useDispatch } from 'react-redux'
-import { openSidebar } from '../utils/appSlice'
+import React, { useEffect } from "react";
+import ButtonList from "./ButtonList";
+import VideoContainer from "./VideoContainer";
+import { useDispatch } from "react-redux";
+import { closeWatchPage } from "../utils/appSlice";
 
 const MainContainer = () => {
-  const dispatch = useDispatch()
-  useEffect(() =>{
-    dispatch(openSidebar())
-  })
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeWatchPage());
+  });
   return (
-    <div>
+    <div className="dark:bg-[#0f0f0f]">
       <ButtonList />
       <VideoContainer />
-
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
+
