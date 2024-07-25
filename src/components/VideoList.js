0bef4c45ba69e248cd59294eef1681da2350ba25
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 15:52:28 +0530

    dark mode updates

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,27 +1,33 @@
-import { Clock } from 'lucide-react'
-import moment from 'moment'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Clock } from "lucide-react";
+import moment from "moment";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const VideoList = ({data}) => {
+const VideoList = ({ data }) => {
   return (
-    <Link to={"/watch?v=" + data.id.videoId}>
-    <div className='md:flex m-4 p-2 hover:cursor-pointer'>
-      <img className='md:w-[450px] md:h-[250px] rounded-lg mr-4 object-cover ' src={data?.snippet.thumbnails.medium.url} alt='thumbnail'/>
-      <div>
-        <h2 className='font-bold  md:text-xl mt-3'>{data.snippet.title}</h2>
-        {/* <p>{data.snippet}</p> */}
+    <Link to={"/watch?v=" + data?.id?.videoId}>
+      <div className="md:flex m-4 p-2 hover:cursor-pointer ml-6">
+        <img
+          className="md:w-[450px] md:h-[250px] rounded-lg mr-4 object-cover "
+          src={data?.snippet?.thumbnails?.medium?.url}
+          alt="thumbnail"
+        />
         <div>
-            {/* <img src=''/> */}
-            <p className='mt-3 font-semibold'>{data.snippet.channelTitle}</p>
-            
-          <p className='font-light flex mt-2 '><Clock className='mr-2 font-light w-6'/>{moment(data?.snippet?.publishedAt).fromNow()}</p>
-          
+          <h2 className="font-bold dark:text-[#fff]  md:text-xl mt-3">
+            {data?.snippet?.title}
+          </h2>
+          <div className="dark:text-gray-300">
+            <p className="mt-3 font-semibold">{data?.snippet?.channelTitle}</p>
+
+            <p className="font-light flex mt-2 ">
+              <Clock className="mr-2 font-light w-6" />
+              {moment(data?.snippet?.publishedAt).fromNow()}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </Link>
-  )
-}
+  );
+};
 
-export default VideoList
+export default VideoList;
