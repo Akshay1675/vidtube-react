85d98070c65a7d9c3c21ae3fc872cf543950ed61
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 15:23:02 +0530

    added custom hook

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,31 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { YOUTUBE_API_KEY } from '../utils/constants'
-import VideoList from './VideoList'
-import { useSearchParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { closeSideBar, openSidebar } from '../utils/appSlice'
+import VideoList from "./VideoList";
+import useSearchResults from "../utils/hooks/useSearchResults";
 
 const SearchContainer = () => {
-    const dispatch = useDispatch()
-    const [searchResults, setSearchResults] = useState([])
-    const [search] = useSearchParams()
-    const query = search.get("search_query")
-    useEffect(() => {
-        getSearchList()
-        dispatch(openSidebar())
-    }, [query])
+  const searchResults = useSearchResults();
 
-    const getSearchList = async () => {
-        const data = await fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + query + "&type=video&key=" + YOUTUBE_API_KEY)
-        const json = await data.json()
-        setSearchResults(json.items)
-    }
   return (
-    <div>
-      {searchResults?.map((result) => <VideoList data={result}/>)}
-      
+    <div className="dark:bg-[#0f0f0f]">
+      {searchResults?.map((result) => (
+        <VideoList data={result} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SearchContainer
+export default SearchContainer;
