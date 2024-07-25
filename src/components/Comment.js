aa6d95dce3ee5a03db3e0ce3bab85b6b84e37077
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-25 12:35:29 +0530

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,22 +1,17 @@
-
-const Comment = ({data}) => {
-
-    
+const Comment = ({ data }) => {
   return (
-    
-    <div className="flex m-2 p-2 bg-slate-50  ">
-      <img className="w-10 h-10 rounded-full" alt="user" src={data.profilePhoto}/>
+    <div className="flex m-2 p-2 bg-slate-50 dark:bg-[#0f0f0f]  ">
+      <img
+        className="w-10 h-10 rounded-full"
+        alt="user"
+        src={data.profilePhoto}
+      />
       <div className="mx-2 font-semibold">
         <p className="text-sm text-pink-700">@{data.name}</p>
-        <p>{data.comment}</p>
+        <p className="dark:text-[#fff]">{data.comment}</p>
       </div>
     </div>
-    
+  );
+};
 
-
-
-
-  )
-}
-
-export default Comment
+export default Comment;
